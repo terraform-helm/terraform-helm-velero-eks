ffae54764e655e966472483dc0cec72003a779a5
Joseph Ligier <joseph.ligier@accenture.com>
2022-10-25 10:45:00 +0200

    Init commit

---
+++ main.tf
@@ -0,0 +1,101 @@
+locals {
+  eks_oidc_issuer_url   = var.oidc_provider != null ? var.oidc_provider : replace(data.aws_eks_cluster.this.identity[0].oidc[0].issuer, "https://", "")
+  eks_oidc_provider_arn = "arn:${data.aws_partition.this.partition}:iam::${data.aws_caller_identity.this.account_id}:oidc-provider/${local.eks_oidc_issuer_url}"
+}
+
+data "aws_partition" "this" {}
+data "aws_caller_identity" "this" {}
+
+data "aws_eks_cluster" "this" {
+  name = var.cluster_id
+}
+
+data "aws_iam_policy_document" "velero" {
+  statement {
+    actions = [
+      "ec2:CreateSnapshot",
+      "ec2:CreateTags",
+      "ec2:CreateVolume",
+      "ec2:DeleteSnapshot",
+      "ec2:DescribeSnapshots",
+      "ec2:DescribeVolumes",
+    ]
+    resources = ["*"]
+  }
+
+  statement {
+    actions = [
+      "s3:AbortMultipartUpload",
+      "s3:DeleteObject",
+      "s3:GetObject",
+      "s3:ListMultipartUploadParts",
+      "s3:PutObject",
+    ]
+    resources = ["arn:${data.aws_partition.this.partition}:s3:::${var.bucket}/*"]
+  }
+
+  statement {
+    actions   = ["s3:ListBucket"]
+    resources = ["arn:${data.aws_partition.this.partition}:s3:::${var.bucket}"]
+  }
+}
+
+module "helm" {
+  source          = "github.com/terraform-helm/terraform-helm-velero"
+  count           = var.install_helm ? 1 : 0
+  release_version = var.release_version
+  images          = var.images
+  set_values = [
+    {
+      name  = "rbac.serviceAccount.name"
+      value = var.service_account_name
+    },
+    {
+      name  = "serviceAccount.server.create"
+      value = "false"
+    },
+  ]
+  values = [templatefile("${path.module}/helm/velero.yaml", {
+    aws_region = var.region,
+    aws_image  = var.image.aws,
+    bucket     = var.bucket,
+    }
+  )]
+}
+
+resource "kubernetes_service_account_v1" "this" {
+  metadata {
+    name        = var.service_account_name
+    namespace   = var.kubernetes_namespace
+    annotations = { "eks.amazonaws.com/role-arn" : aws_iam_role.this.arn }
+  }
+
+  dynamic "image_pull_secret" {
+    for_each = var.kubernetes_svc_image_pull_secrets != null ? var.kubernetes_svc_image_pull_secrets : []
+    content {
+      name = image_pull_secret.value
+    }
+  }
+
+  automount_service_account_token = true
+}
+
+resource "aws_iam_role" "this" {
+  name                  = var.irsa_iam_role_name
+  description           = "AWS IAM Role for the Kubernetes service account ${var.service_account_name}."
+  assume_role_policy    = data.aws_iam_policy_document.assume_role_policy.json
+  path                  = var.irsa_iam_role_path
+  force_detach_policies = true
+  permissions_boundary  = var.irsa_iam_permissions_boundary
+}
+
+resource "aws_iam_policy" "this" {
+  name        = var.irsa_iam_policy_name
+  description = "Cluster Velero IAM policy"
+  policy      = data.aws_iam_policy_document.velero.json
+}
+
+resource "aws_iam_role_policy_attachment" "this" {
+  policy_arn = aws_iam_policy.this.arn
+  role       = aws_iam_role.this.name
+}
---
+++ variables.tf
@@ -0,0 +1,80 @@
+variable "images" {
+  description = "Map of images"
+  type        = map(string)
+  default     = {}
+}
+
+variable "install_helm" {
+  description = "Do you want to install helm chart?"
+  type        = bool
+  default     = true
+}
+
+variable "release_version" {
+  description = "version of helm release"
+  type        = string
+  default     = null
+}
+
+variable "service_account_name" {
+  description = "Name of the service account to have right to send to S3 bucket"
+  type        = string
+  default     = "velero"
+}
+
+variable "kubernetes_svc_image_pull_secrets" {
+  description = "Secrets to pull your image"
+  type        = list(any)
+  default     = null
+}
+
+variable "kubernetes_namespace" {
+  description = "Namespace to install autoscaler pod"
+  type        = string
+  default     = "velero"
+}
+
+variable "irsa_iam_role_name" {
+  type        = string
+  description = "IAM role name for IRSA"
+  default     = "eks-velero"
+}
+
+variable "irsa_iam_policy_name" {
+  type        = string
+  description = "IAM policy name for IRSA"
+  default     = "eks-velero"
+}
+
+variable "irsa_iam_permissions_boundary" {
+  description = "IAM permissions boundary for IRSA roles"
+  type        = string
+  default     = ""
+}
+
+variable "irsa_iam_role_path" {
+  description = "IAM role path for IRSA roles"
+  type        = string
+  default     = "/"
+}
+
+variable "cluster_id" {
+  description = "EKS cluster name"
+  type        = string
+}
+
+variable "bucket" {
+  description = "bucket name"
+  type        = string
+}
+
+variable "region" {
+  description = "Region of you eks cluster"
+  type        = string
+}
+
+variable "oidc_provider" {
+  description = "EKS OIDC provider"
+  type        = string
+  default     = null
+}
